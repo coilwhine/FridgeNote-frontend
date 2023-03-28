ab239d882a77d8a8ffb483b0f9602321acc7099a
coilwhine <hen.daniel47@gmail.com>
2023-03-28 11:54:31 +0300

    test

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>asdasfst</h1>
+      <h3>test</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem reprehenderit molestiae sapiente eaque, quam a, maiores itaque hic earum recusandae, eum repellat asperiores voluptatibus laudantium quia dolor. Molestiae, ab? Minus!</p>
+    </div>
+  )
+}
+
+export default App
